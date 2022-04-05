8748778724ae676f1b237826ca2d8d42eb85dfde
CarolinaVanuchi <carolinavanuchi21@gmail.com>
2022-04-04 22:46:15 -0300

    listando caadastro

--- src/components/manual/tableList.tsx
+++ src/components/manual/tableList.tsx
@@ -1,35 +1,56 @@
-import React from "react";
-import { Table } from 'react-bootstrap';
+import React from 'react';
+import { RequesterServiceModel, RequesterMethodEnum } from "../../api/api";
+import Requester from '../../api/request';
+import { Table, Button } from 'react-bootstrap';
+import { FaTrash, FaRegHandPointUp } from 'react-icons/fa';
+class TableList extends React.Component {
 
-const TableList = () => {
-    return (
-        <>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>Nome</th>
-                        <th>Base</th>
-                        <th>Primeiro Elo</th>
-                        <th>Segundo Elo</th>
-                        <th>Excluir</th>
-                        <th>Selecionar</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                        
-                       
-                    </tr>
-                </tbody>
-            </Table>
-        </>
-    );
+    state = {
+        itensList: []
+    };
+
+    async componentDidMount() {
+        const service: RequesterServiceModel = {
+            method: RequesterMethodEnum.GET,
+            endpoint: '/all'
+        }
+        const { data } = await Requester(service);
+        this.setState({
+            itensList: data
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Base</th>
+                            <th>Primeiro Elo</th>
+                            <th>Segundo Elo</th>
+                            <th>Excluir</th>
+                            <th>Selecionar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.itensList.map(item => (
+                            <tr key={item.id_angle}>
+                                <td>{item.description}</td>
+                                <td>{item.theta1}</td>
+                                <td>{item.theta2}</td>
+                                <td>{item.theta3}</td>
+                                <td><Button size="sm" variant='danger'><FaTrash /></Button></td>
+                                <td><Button size="sm"><FaRegHandPointUp /></Button></td>
+                            </tr>
+                        ))}
+
+                    </tbody>
+                </Table>
+            </div>
+        );
+    }
 }
 
 export default TableList;
